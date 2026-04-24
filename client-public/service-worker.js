e6515db5054efdaf94172fee07911a42421634cf
davkchit <davkchv@gmail.com>
2026-04-24 12:50:38 +0300

    new-new

--- client-public/service-worker.js
+++ client-public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'v1';
+const CACHE_VERSION = 'v2';
 const SHELL_CACHE = `anti-vuz-shell-${CACHE_VERSION}`;
 const STATIC_CACHE = `anti-vuz-static-${CACHE_VERSION}`;
 const DATA_CACHE = `anti-vuz-data-${CACHE_VERSION}`;
